400059dcf3353a5fb220b5380243e41f5c738a2e
Aneer Fidalgo <aneer@Aneers-Air.hsd1.fl.comcast.net>
2022-06-28 21:17:11 -0400

    added characters and text

---
+++ src/components/Quote.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Quote() {
+  return (
+    <div className="quote">
+      <div className="anime" name="Tanjiro Kamado">
+        Tanjiro Kamado
+      </div>
+
+      <blockquote>
+        i can do it, i know i can do it. i'm the guy who gets it done broken
+        bones or not... no matter what... I CAN DO IT! I CAN FIGHT!!
+      </blockquote>
+
+      <div className="character" name="Ichigo Kurosaki">
+        Ichigo Kurosaki
+      </div>
+    </div>
+  );
+}
+
+export default Quote;
